66c6b32054390d22045c7a08f87836412a3e5f0f
Pidgeey <ludwig.somo@gmail.com>
2018-02-13 11:28:24 +0100

    Ajout BDD et js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
+(function main(){
+var date;
+var commande;
+var cptclient;
+var client;
 
-// $(".connexion-button").click(function(){
-	
-// });
+	$.get('bdd.json', {}, function(data){
+
+		for(var i= 0; i<data.commandes.length; i++){
+		date = data.commandes[i].date
+		commande = data.commandes[i].numcommande
+		cptclient = data.commandes[i].numcptclient
+		client = data.commandes[i].nomclient
+		var chk = '<td><input type="checkbox"></td>'
+		var btn = '<td><button>Modifier</button></td>'
+		$('#tasty').append("<tr><td>"+date+"</td><td>"+commande+"</td><td>"+cptclient+"</td><td>"+client+"</td>"+chk+chk+chk+chk+chk+chk+btn+"</td></tr>")
+		
+	}
+		// $('#template').html(Mustache.render($('#template').html(), {test : data.commandes} ))
+		// console.log("test");
+	}, 'json');
+})();
