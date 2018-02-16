3f4112f301baba66da84bbda85feb64cf5fa5e41
Pidgeey <ludwig.somo@gmail.com>
2018-02-16 09:59:54 +0100

    bdd change

---
+++ detailcmd.js
@@ -0,0 +1,35 @@
+{
+	"date": "12/01/18", //Date de la commande
+	"cmdnumb": "18927578", // Numéro de commande
+	"accnumb": "8027618", //Numéro de compte Client
+	"clientname": "Mme Pierre", //Nom du client
+	"commercial": "Micka", //Nom du commercial/revendeur
+	"urgency": true, // Urgence - true=oui / false=non. urgence peut etre oui ou bien une date 
+	"info": "imprimante", // Commande additionelles
+	"eligst": true, // Eligible S/T true=oui / false=non
+	"valist": true, // Validation S/T true=oui / false=non
+	"bcb": "Reçu", // Réception du BCB		 
+	"tsft": "****", // Transfert
+	"color": "Vert", // Couleur etui
+	"credit": true, // Crédit true=oui / false=non
+	"try": true, // Essai
+	"destlivraison": "Alliance Santé", // Destinataire de livraison
+	"ekosimply": "Simply", // Produit Simply ou Eko
+	"domtom": false, // Livraison vers Dom Tom true=oui / false=non
+	"dateparam": "14/07", // Date de prise en charge au service paramétrage
+	"techparam": "Jean-Jacques", // Technicien paramétrage
+	"datehard": "15/07", // Date de prise en charge au service hard
+	"techhard": "Michel", // Technicien hard
+	"numtrack": "8CJEJ863409", // Numéro de tracking
+	"expdate": "17/08", // Date d'expédition
+	"returntracknum": "***", // Numéro de tracking retour
+	"step"	: // Etapes d'avancement
+		[
+			1, // ST
+			1, // BCB
+			1, // Crédit
+			0, // Param
+			0, // Prépa
+			0  // Livraison
+		]
+}
