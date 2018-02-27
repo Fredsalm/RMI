dd37d97b44ba7ab9a8161faaa314f8201e2212a3
Pidgeey <ludwig.somo@gmail.com>
2018-02-27 15:32:34 +0100

    Commit final

--- js/app.js
+++ js/app.js
@@ -4,40 +4,59 @@
 		var $tableau = $('#tableau');
 
 		$.get('bdd.json', {}, function(data){
-                console.log("test")
+            
+
+			//Création du tableau
 			for(var i= 0; i<data.commandes.length; i++){
 				var html = '',
 					cmd = data.commandes[i],
-					step = cmd.step;
+					step = cmd.step,
+					urgency = "",
+					stbl = cmd.sortable,
+					urgtry = "";
 
-				html += '<td class="ref">'+cmd.date+'</td>';
-				html += '<td class="ref">'+cmd.cmdnumb+'</td>';
-				html += '<td class="ref">'+cmd.accnumb+'</td>';
-				html += '<td class="ref">'+cmd.clientname+'</td>';
+
+				if(cmd.urgency[0].selected == "selected"){
+					urgency = "urgency"; // Condition Urgence. 
+				};
+
+				if(cmd.urgency[1].selected == "selected" && cmd.try[0].selected == "selected"){
+					urgtry = "try"; // Condition essai. 
+				}
+
+				html += '<td>'+cmd.date+'</td>';
+				html += '<td>'+cmd.cmdnumb+'</td>';
+				html += '<td>'+cmd.accnumb+'</td>';
+				html += '<td>'+cmd.clientname+'</td>';
 				
-				html += '<td class="chk">';
+				html += '<td sorttable_customkey="'+stbl+'">';
 				for (var j = 0; j < step.length; j++) {
-					html += '<div class="step ' + (step[j] ? 'ok' : 'wait') + '"></div>';
+					html += '<div class="step ' + (step[j] ? 'ok' : 'wait') + '"></div>'; //Condition Couleurs Puces
 				}
 				html += '</td>';
 				html += '<td><a href="' + $tableau.data('url') + '?c='+cmd.cmdnumb+'">Modifier</a></td>';
 
-				$('tbody', $tableau).append('<tr class="'+cmd.state+'">' + html + '</tr>')
+				$('tbody', $tableau).append('<tr class="' + cmd.state + " " + urgency + " " + urgtry + '">' + html + '</tr>')
+				//Lignes Tableau
+
+
 			};
-		}, 'json');
+        }, 'json');
 
+		//Filtres Boutons services
 		$('.filtre button').on('click', function(){
-			var $b = $(this);
-			var filter = $b.data('filter');
+			var $b = $(this),
+				filter = $b.data('filter');
 
+			//Ajout des classes de filtre/tri
 			if(!$b.hasClass('active')){
 
 				$('.filtre .active').add($b).toggleClass('active');
 
-				$('tr.hide', $tableau).removeClass('hide');
+				$('tbody tr.hide', $tableau).removeClass('hide');
 				
 				if(filter != '*')
-					$('tr:not(.'+filter+')').addClass('hide');					
+					$('tbody tr:not(.'+filter+')').addClass('hide');					
 
 			};
 		});
@@ -47,6 +66,7 @@
 
 		var oParametre = {};
 
+		//Récupération du numéro de commande dans l'url
 		if (window.location.search.length > 1) {
 		  for (var aItKey, nKeyId = 0, aCouples = window.location.search.substr(1).split("&"); nKeyId < aCouples.length; nKeyId++) {
 		     aItKey = aCouples[nKeyId].split("=");
@@ -54,7 +74,9 @@
 		    };
 		};
 
+		// Render Mustache
 		$.get('bdd.json', {}, function(data){
+			console.log(oParametre)
 			for(var i=0; i<data.commandes.length; i++){
 				if(data.commandes[i].cmdnumb == oParametre.c ){
 					$('.tpl').html(Mustache.render($('.tpl').html(), {test : data.commandes[i]}))
